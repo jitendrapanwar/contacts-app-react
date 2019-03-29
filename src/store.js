9675b1a6ecb855e96eea905025546147316c618b
jitendrapanwar <gm.jitendra@gmail.com>
2019-03-29 14:37:46 +0530

    updating store with rootReducer

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger'
-import contactReducer from './reducers/contact'
+import rootReducer from './reducers/index'
 const store = createStore(
-  contactReducer,
+  rootReducer,
   applyMiddleware(thunk, logger)
 )
 
