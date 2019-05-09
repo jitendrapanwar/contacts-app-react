ffabf30ab423b1a1ace6de06e2be36ef0fafab59
jitendrapanwar <gm.jitendra@gmail.com>
2019-05-09 11:14:34 +0530

    adding info icon

--- src/components/contact-item.js
+++ src/components/contact-item.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ListItemText from '@material-ui/core/ListItemText';
-
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
 function getFullName(contact) {
   return `${contact.first} ${contact.last}`
 }
 const ContactItem = (props) => (
   <div>
     <ListItemText primary={getFullName(props.contact.name)} />
+    <ListItemSecondaryAction>
+      <IconButton aria-label="Comments">
+        <InfoIcon />
+      </IconButton>
+    </ListItemSecondaryAction>
   </div>
 )
 
