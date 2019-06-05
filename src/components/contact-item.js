a8277266aae0a24ad7c834f37bea93857722768c
jitendrapanwar <gm.jitendra@gmail.com>
2019-06-05 12:44:13 +0530

    adding avatar in list

--- src/components/contact-item.js
+++ src/components/contact-item.js
@@ -3,18 +3,25 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+
 function getFullName(contact) {
   return `${contact.first} ${contact.last}`
 }
+
 const ContactItem = (props) => (
-  <div>
-    <ListItemText primary={getFullName(props.contact.name)} />
+  <React.Fragment>
+    <ListItemAvatar>
+      <Avatar src={props.contact.picture.thumbnail}></Avatar>
+    </ListItemAvatar>
+    <ListItemText primary="" secondary={getFullName(props.contact.name)} />
     <ListItemSecondaryAction>
       <IconButton aria-label="Comments">
         <InfoIcon />
       </IconButton>
     </ListItemSecondaryAction>
-  </div>
+  </React.Fragment>
 )
 
 export default ContactItem;
